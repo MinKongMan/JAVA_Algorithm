ee0e79eceee8489a3259e58622ca8fc25d5d0345
MinKongMan <69151872+MinKongMan@users.noreply.github.com>
2021-04-05 15:31:39 +0900

    Add files via upload
    백준 1351 무한수열 / Hashmap + DP
    1. 데이터가 100억이 넘는 수 까지 와서 long형으로 받아야 했음.
    2. 인덱스가 long형이라 배열을 선언하면 인덱스가 따라오질 못함, 그리고 메모리도 초과됨.
    3. 배열을 사용할 수 없어 Hashmap을 이용
    4. Hashmap도 늘 풀어왔던 dp처럼 for문을 1부터 N까지 실행하는게 아니라, 재귀문으로 필요한 값만 저장한다.
     4-1) ex) N이 1억이라 하면, 1~1억까지 계산을 해야겠지만 재귀문을 돌리면 M과 K값에 따라 저장되는 최대 크기가 달라진다.
     4-2) 물론 M과 K값 중 큰 값을 골라 거기까지 for문을 돌리고 최종 답을 구해도 될거같긴 하다.

---
+++ DP/Baek_1351_infiniteSequence_210405.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Baek_1351_infiniteSequence_210405 {
+	static long N,M,K;
+	static HashMap<Long,Long> hash;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		N = Long.parseLong(st.nextToken());
+		M = Long.parseLong(st.nextToken());
+		K = Long.parseLong(st.nextToken());
+		hash = new HashMap<Long,Long>();
+		hash.put(0L, 1L);
+		System.out.println(find(N));
+	}
+	
+	static long find(long N) {
+		if(hash.containsKey(N)) {
+			return hash.get(N);
+		}
+		else {
+			long val = find(N/K)+find(N/M);
+			hash.put(N, val);
+			return val;
+		}
+	}
+}
