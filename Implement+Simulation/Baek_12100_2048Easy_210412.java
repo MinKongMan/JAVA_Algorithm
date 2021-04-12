9bc110f03b115175906f6d1cc7407139da4a9fcc
MinKongMan <69151872+MinKongMan@users.noreply.github.com>
2021-04-12 22:43:46 +0900

    Add files via upload
    백준 12100 2048 문제 / 구현 / 55분
    백트래킹을 이용하여 구현을 하는 문제다.
    
    왼쪽으로 몰빵하는 경우를 생각하면,
    하나의 행을 기준을 잡고, 인덱스 j = 2부터 N-1까지 탐색을 한다.
    array[i][j-1]이 0이고, array[i][j]가 0이 아니면 array[i][j-1] = array[i][j], array[i][j] = 0을 조건을 걸고,
    array[i][j]가 0이거나 array[i][j] array[i][j-1]둘다 0이 아니면 j++
    이러한 반복문을 만들어 내 왼쪽편에 0이 존재하지 않도록 다 밀어준다.
    
    다음
    합치는 알고리즘을 진행하는데,
    j = 1부터 j<=N-1까지
    array[i][j]== array[i][j+1]이면
    array[i][j] = 2*array[i][j], array[i][j+1] = 0, j = j+1의 조건을 건다.
    j = j+1을 해주는 이유는 이미 다음 열을 0으로 만들어 주는 과정까지 진행했기 때문에 그 다음 열까지 진행하는 것이다.
    
    마지막으로 처음에 해줬던 0없애는 작업을 진행하는 하나의 작업을 하나의 행에 적용 시키면 된다.

---
+++ Implement+Simulation/Baek_12100_2048Easy_210412.java
@@ -0,0 +1,230 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baek_12100_2048Easy_210412 {
+	static int[][] array,array_cpy;
+	static int N;
+	static int temp = 0, max = 0;
+	static boolean[] marked;
+	static int[] array2;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		array = new int[N+1][N+1];
+		array_cpy = new int[N+1][N+1];
+		StringTokenizer st;
+		for(int i = 1; i<=N; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			for(int j = 1; j<=N; j++) {
+				array_cpy[i][j] = Integer.parseInt(st.nextToken());
+				array[i][j] = array_cpy[i][j];
+			}
+		}
+		marked = new boolean[6];
+		array2 = new int[6];
+//		func1();
+//		for(int i = 1; i<=N; i++) {
+//			for(int j = 1; j<=N; j++) {
+//				System.out.print(array[i][j]+" ");
+//			}
+//			System.out.println();
+//		}
+		
+		find(1);
+		System.out.println(max);
+	}
+	static void find(int a) {
+		if(a==6) {
+			temp = 0;
+			for(int i = 1; i<=5; i++) {
+				if(array2[i]==1) func1();
+				else if(array2[i]==2) func2();
+				else if(array2[i]==3) func3();
+				else if(array2[i]==4) func4();
+			}
+			for(int i= 1; i<=N; i++) {
+				for(int j = 1; j<=N; j++) {
+					temp = temp>array[i][j]?temp:array[i][j];
+					array[i][j] = array_cpy[i][j];
+				}
+			}
+			max = max>temp?max:temp;
+			return;
+		}
+		for(int i = 1; i<=4; i++) {
+			if(!marked[a]) {
+				marked[a] = true;
+				array2[a] = i;
+				find(a+1);
+				marked[a] = false;
+			}
+		}
+	}
+	
+	static void func1() {
+		for(int i = 1; i<=N; i++) {
+			int j = 2;
+			while(j<=N) {
+				if(array[j][i]==0) {
+					j++;
+					continue;
+				}
+				else if(j>=2 && array[j][i]!=0 && array[j-1][i]==0){
+					array[j-1][i] = array[j][i];
+					array[j][i] = 0;
+					j = j-1;
+				}
+				else if(array[j][i]!=0 && array[j][i]!=0) {
+					j++;
+				}
+			}
+			for(j = 1; j<N; j++) {
+				if(array[j][i]==array[j+1][i]) {
+					array[j][i] = 2*array[j][i];
+					array[j+1][i] = 0;
+					j = j+1;
+				}
+			}
+			j = 2;
+			while(j<=N) {
+				if(array[j][i]==0) {
+					j++;
+					continue;
+				}
+				else if(array[j][i]!=0 && array[j-1][i]==0){
+					array[j-1][i] = array[j][i];
+					array[j][i] = 0;
+					j = j-1;
+				}
+				else if(array[j][i]!=0 && array[j][i]!=0) {
+					j++;
+				}
+			}
+		}
+	}
+	static void func2() {
+		for(int i = 1; i<=N; i++) {
+			int j = N-1;
+			while(j>=1) {
+				if(array[i][j]==0) {
+					j--;
+					continue;
+				}
+				else if(j<=N-1 && array[i][j]!=0 && array[i][j+1]==0){
+					array[i][j+1] = array[i][j];
+					array[i][j] = 0;
+					j = j+1;
+				}
+				else if(array[i][j]!=0 && array[i][j]!=0) {
+					j--;
+				}
+			}
+			for(j = N; j>1; j--) {
+				if(array[i][j]==array[i][j-1]) {
+					array[i][j] = 2*array[i][j];
+					array[i][j-1] = 0;
+					j = j-1;
+				}
+			}
+			j = N-1;
+			while(j>=1) {
+				if(array[i][j]==0) {
+					j--;
+					continue;
+				}
+				else if(array[i][j]!=0 && array[i][j+1]==0){
+					array[i][j+1] = array[i][j];
+					array[i][j] = 0;
+					j = j+1;
+				}
+				else if(array[i][j]!=0 && array[i][j]!=0) {
+					j--;
+				}
+			}
+		}
+	}
+	static void func3() {
+		for(int i = 1; i<=N; i++) {
+			int j = N-1;
+			while(j>=1) {
+				if(array[j][i]==0) {
+					j--;
+					continue;
+				}
+				else if(j <= N-1 && array[j][i]!=0 && array[j+1][i]==0){
+					array[j+1][i] = array[j][i];
+					array[j][i] = 0;
+					j = j+1;
+				}
+				else if(array[j][i]!=0 && array[j][i]!=0) {
+					j--;
+				}
+			}
+			for(j = N; j>1; j--) {
+				if(array[j][i]==array[j-1][i]) {
+					array[j][i] = 2*array[j][i];
+					array[j-1][i] = 0;
+					j = j-1;
+				}
+			}
+			j = N-1;
+			while(j>=1) {
+				if(array[j][i]==0) {
+					j--;
+					continue;
+				}
+				else if(array[j][i]!=0 && array[j+1][i]==0){
+					array[j+1][i] = array[j][i];
+					array[j][i] = 0;
+					j = j+1;
+				}
+				else if(array[j][i]!=0 && array[j][i]!=0) {
+					j--;
+				}
+			}
+		}
+	}
+	static void func4() {
+		for(int i = 1; i<=N; i++) {
+			int j = 2;
+			while(j<=N) {
+				if(array[i][j]==0) {
+					j++;
+					continue;
+				}
+				else if(j>=2 && array[i][j]!=0 && array[i][j-1]==0){
+					array[i][j-1] = array[i][j];
+					array[i][j] = 0;
+					j = j-1;
+				}
+				else if(array[i][j]!=0 && array[i][j]!=0) {
+					j++;
+				}
+			}
+			for(j = 1; j<N; j++) {
+				if(array[i][j]==array[i][j+1]) {
+					array[i][j] = 2*array[i][j];
+					array[i][j+1] = 0;
+					j = j+1;
+				}
+			}
+			j = 2;
+			while(j<=N) {
+				if(array[i][j]==0) {
+					j++;
+					continue;
+				}
+				else if(array[i][j]!=0 && array[i][j-1]==0){
+					array[i][j-1] = array[i][j];
+					array[i][j] = 0;
+					j = j-1;
+				}
+				else if(array[i][j]!=0 && array[i][j]!=0) {
+					j++;
+				}
+			}
+		}
+	}
+}
