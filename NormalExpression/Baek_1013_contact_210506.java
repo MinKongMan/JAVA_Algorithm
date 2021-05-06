d7cb0bec5786f3a25b46fada5ed919a3c123625a
MinKongMan <69151872+MinKongMan@users.noreply.github.com>
2021-05-06 19:50:17 +0900

    Add files via upload
    백준 1013 contact / 정규표현식
    
    정규 표현식을 이용하여 푼 문제
    Pattern 클래스의 matches함수를 이용하여 정규식, 받아온 식을 비교하여 푼 문제
    
    정규 표현식을 다룰줄 알면 문제 풀기가 훨 수월한걸 느꼈다.

---
+++ NormalExpression/Baek_1013_contact_210506.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+public class Baek_1013_contact_210506 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i<=N; i++) {
+			String l = br.readLine();
+			boolean check = Pattern.matches("^(100+1+|01)+$", l);
+			if(check) sb.append("Yes\n");	
+			else sb.append("No\n");
+		}
+		System.out.println(sb);
+	}
+
+}
