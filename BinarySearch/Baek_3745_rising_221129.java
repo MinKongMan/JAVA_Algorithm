db3c8b52c504b6bd7b77defbc7b9eb1c379df6ab
강민광 <69151872+MinKongMan@users.noreply.github.com>
2022-11-29 07:48:32 +0900

    Add files via upload
    백준 이분탐색
    1. EOF를 제대로 알자.
    2. 같은 수가 나올 때 r의 부분에서 신경을 써주자.
        -> l부분에서 신경을 쓰게 되면 수가 의미없이 추가된다.
        최종적으로 내가 짠 코드는 l+1이 된 부분에서 수를 바꿔주기 때문에 l부분에서 같은 수를 맞춰주게 되면 오류가 생긴다.

---
+++ BinarySearch/Baek_3745_rising_221129.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Baek_3745_rising_221129 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		StringBuilder sb = new StringBuilder();
+		String line = "";
+		int count = 0;
+		while((line = br.readLine()) != null) {
+		    if(count>=1) break;int N = Integer.parseInt(line.trim());
+		    st = new StringTokenizer(br.readLine().trim());
+		    
+			int[] array = new int[N+1];
+			
+			for(int i = 1; i<=N; i++) {
+				array[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			ArrayList<Integer> ar = new ArrayList<Integer>();
+			
+			for(int i = 1; i<=N; i++) {
+				if(ar.size()==0) ar.add(array[i]);
+				else {
+					int l = 0;
+					int r = ar.size()-1;
+					
+					if(array[i]>ar.get(r)) ar.add(array[i]);
+					else {
+						int mid = 0;
+						while(l<r) {
+							mid = (l+r)/2;
+							if(ar.get(mid)>array[i]) r = mid;
+//							else if(ar.get(mid)==array[i]) break;
+							else l = mid+1;
+						}
+						ar.set(l, array[i]);
+					}
+				}
+				
+			}
+			count++;
+			sb.append(ar.size()+"\n");
+		}
+		System.out.println(sb);
+		
+		
+
+	}
+
+}
